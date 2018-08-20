4ccdd3582cbc351621b628637a0a5a1a1df1d09b
ArnaudPec <arnaud.pecoraro@utt.fr>
2018-08-20 20:53:24 +0200

    Add API comments

--- main/ht16k33_7seg.h
+++ main/ht16k33_7seg.h
@@ -1,23 +1,52 @@
 #ifndef HT16K33_7SEG_H
 #define HT16K33_7SEG_H
 
-void ht16k33_set_blink_rate(uint8_t b);
-void ht16k33_set_brightness(uint8_t b);
-void ht16k33_write_digit_raw(uint8_t d, uint8_t bitmask);
-void ht16k33_write_digit_num(uint8_t d, uint8_t num, uint8_t dot);
-void ht16k33_write_display(void);
-void ht16k33_clear(void);
-void ht16k33_draw_colon(uint8_t state);
+
+/**
+ * @brief Init ht16k33 i2c component
+ */
 void ht16k33_init(uint8_t sda_pin, uint8_t scl_pin);
 
-enum ht16k33_blink_level
-{
-    HT16K33_BLINK_OFF = 0,
-    HT16K33_BLINK_2HZ,
-    HT16K33_BLINK_1HZ,
-    HT16K33_BLINK_HALFHZ
-};
+/**
+ * @brief Set display blinking rate
+ *
+ * @see ht16k33_blink_level
+ */
+void ht16k33_set_blink_rate(uint8_t b);
 
+/**
+ * @brief Set display brightness
+ */
+void ht16k33_set_brightness(uint8_t b);
+
+/**
+ * @brief Write raw datas to display buffer
+ */
+void ht16k33_write_digit_raw(uint8_t d, uint8_t bitmask);
+
+/**
+ * @brief Write number to display buffer
+ */
+void ht16k33_write_digit_num(uint8_t d, uint8_t num, uint8_t dot);
+
+/**
+ * @brief Write buffers on display
+ */
+void ht16k33_write_display(void);
+
+/**
+ * @brief Clear the display
+ */
+void ht16k33_clear(void);
+
+/**
+ * @brief Draw colon
+ */
+void ht16k33_draw_colon(uint8_t state);
+
+/**
+ * @brief Write formated datas to display buffer
+ */
 #define ht16k33_print(x, base)                                                    \
     __builtin_choose_expr (                                                       \
             __builtin_types_compatible_p (typeof (x), double),                    \
@@ -40,11 +69,27 @@ enum ht16k33_blink_level
                             (void)0))))))
 
 
+/**
+ * @brief Write formated datas to display buffer after return to the biginning of the line
+ */
 #define ht16k33_println(x, base) \
 { \
     ht16k33_print(x, base); \
     _println(); \
 }
+
+/**
+ * @enum blink level
+ *
+ * @see ht16k33_set_blink_rate
+ */
+enum ht16k33_blink_level
+{
+    HT16K33_BLINK_OFF = 0,
+    HT16K33_BLINK_2HZ,
+    HT16K33_BLINK_1HZ,
+    HT16K33_BLINK_HALFHZ
+};
 
 void _print_char(char c, uint8_t base);
 void _print_uchar(unsigned char b, uint8_t base);
